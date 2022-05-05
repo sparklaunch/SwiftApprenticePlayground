c3f17cd563b6ae6fb1305757f65c23276e0c50e9
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-05 18:47:32 +0900

    Initial Commit

---
+++ Chapter00.playground/Contents.swift
@@ -0,0 +1,26 @@
+// TASK 1.
+let myAge = 31
+
+// TASK 2.
+var averageAge = 31.0
+
+averageAge += 30.0
+averageAge /= 2
+
+print(averageAge)
+
+// TASK 3.
+let testNumber = 32
+let evenOdd = testNumber % 2
+
+print(evenOdd)
+
+// TASK 4.
+var answer = 0
+
+answer += 1
+answer += 10
+answer *= 10
+answer >>= 3
+
+print(answer)
