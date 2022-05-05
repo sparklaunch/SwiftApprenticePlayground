74d6bd34f52f66d031c36affbc3dcda02a3582a8
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-06 07:38:35 +0900

    Initialize.

---
+++ Chapter04.playground/Contents.swift
@@ -0,0 +1,13 @@
+// TASK 1.
+let date = (month: 5, day: 6, year: 2022, averageTemperature: 12.0)
+
+// TASK 2.
+let (month, day, year, averageTemperature) = date
+
+// TASK 3.
+let (_, day2, _, averageTemperature2) = date
+
+// TASK 4.
+var date2 = (month: 5, day: 6, year: 2022, averageTemperature: 12.0)
+
+date2.averageTemperature = 14.0
