7958c34a8fbaaaa6a1b7d18fb088dbd06c135324
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-06 07:31:17 +0900

    Initialize.

---
+++ Chapter03.playground/Contents.swift
@@ -0,0 +1,11 @@
+// TASK 1.
+let firstName = "Jinwook"
+let lastName = "Kim"
+
+// TASK 2.
+let fullName = firstName + " " + lastName
+
+// TASK 3.
+let myDetails = "Hello, my name is \(fullName)."
+
+print(myDetails)
