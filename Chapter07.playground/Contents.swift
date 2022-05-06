c8b741f633de30b32f7497d8adca0b25a70933fb
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-06 10:48:07 +0900

    Initialize.

---
+++ Chapter07.playground/Contents.swift
@@ -0,0 +1,13 @@
+// TASK 1.
+let myAge = 31
+
+if myAge >= 13 && myAge <= 19 {
+    print("Teenager")
+} else {
+    print("Not a teenager")
+}
+
+// TASK 2.
+let answer = myAge >= 13 && myAge <= 19 ? "Teenager" : "Not a teenager"
+
+print(answer)
