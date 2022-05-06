2cb54f29eed68b85056aeb5002cc804a55c16354
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-06 10:39:42 +0900

    Initialize.

---
+++ Chapter06.playground/Contents.swift
@@ -0,0 +1,24 @@
+// TASK 1.
+let myAge = 31
+let isTeenager = myAge >= 13 && myAge <= 19
+
+print(isTeenager)
+
+// TASK 2.
+let theirAge = 30
+let bothTeenagers = isTeenager && (theirAge >= 13 && theirAge <= 19)
+
+print(bothTeenagers)
+
+// TASK 3.
+let reader = "Jinwook Kim"
+let author = "Matt Galloway"
+
+let authorIsReader = reader == author
+
+print(authorIsReader)
+
+// TASK 4.
+let readerBeforeAuthor = reader < author
+
+print(readerBeforeAuthor)
