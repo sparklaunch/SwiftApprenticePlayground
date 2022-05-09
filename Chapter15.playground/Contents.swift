ade57cee7ee90ed0c632f629c11763a898717448
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-10 08:52:27 +0900

    Initialize.

---
+++ Chapter15.playground/Contents.swift
@@ -0,0 +1,14 @@
+// TASK 1.
+let myFavoriteSong: String?
+
+myFavoriteSong = "The man who can't be moved"
+
+// TASK 2.
+let parsedInt = Int("10")
+
+print(parsedInt)
+
+// TASK 3.
+let parsedInt2 = Int("dog")
+
+print(parsedInt2)
