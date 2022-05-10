0bafcc488cd6114362aebdb89bde3661a2e011da
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-10 11:51:03 +0900

    Initialize.

---
+++ Chapter20.playground/Contents.swift
@@ -0,0 +1,9 @@
+// TASK 1.
+var gina = [
+    "name": "Gina",
+    "score": "3",
+    "city": "Missisauga",
+    "state": "Saskatchewan"
+]
+
+print("\(gina["name"]!) lives in \(gina["city"]!), \(gina["state"]!).")
