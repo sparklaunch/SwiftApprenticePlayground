cb6300d322a1a37d9d189f9670caf81c1aad20fd
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-10 11:35:23 +0900

    Initialize.

---
+++ Chapter19.playground/Contents.swift
@@ -0,0 +1,7 @@
+// TASK 1.
+var players = ["Gina", "Dan", "Todd", "Ben", "Frank", "Bob"]
+var scores = [2, 2, 8, 6, 1, 2]
+
+for (index, player) in players.enumerated() {
+    print("\(player): \(scores[index])")
+}
