3349cc429a3fb0af7479d8b4e65a33b905704b9a
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-23 11:48:24 +0900

    Initialize.

---
+++ Chapter45.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+// TASK 1.
+enum Coin: Int {
+    case penny = 1
+    case nickel = 5
+    case dime = 10
+    case quarter = 25
+}
+
+let coinPurse: [Coin] = [.penny, .nickel, .dime, .quarter]
